38ad52daf018328772c48e2f7bfcaa5cac7971d3
aakashvishwakarma <aakashvishwakarma72@gmail.com>
2021-08-24 17:17:41 +0530

    Changes

--- Dev/main.tf
+++ Dev/main.tf
@@ -16,4 +16,4 @@ module "my_instance" {
   ami_id ="ami-048f6ed62451373d9"
   #instance_type ="t2.micro" // we have already defined default value as t2.micro
   subnet_id = "${module.my_vpc.subnet_id}"
-  }
+  
